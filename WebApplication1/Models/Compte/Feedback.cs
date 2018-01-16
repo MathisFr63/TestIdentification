6152111089a7009b48312c5ac47f639e8ca55b48
aur-berger <34644534+aur-berger@users.noreply.github.com>
2018-01-16 14:35:05 +0100

    ajout feedback

---
+++ WebApplication1/Models/Compte/Feedback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.Compte
+{
+    public class Feedback
+    {
+
+        public String name { get; set; }
+
+        public String email { get; set; }
+
+        public String comment { get; set; }
+    }
+}
