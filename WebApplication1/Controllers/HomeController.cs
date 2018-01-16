aa505aecb0e899821033b0818b55c9b98caec8aa
aur-berger <34644534+aur-berger@users.noreply.github.com>
2018-01-16 15:21:43 +0100

    ajotu feedback

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models.Compte;
 
 namespace WebApplication1.Controllers
 {
